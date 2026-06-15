ab8f8c1e3c81b4d3cb7f39d1b6ae2f083176a87d
26-3103-alt <26-3103@sc.gyo6.net>
2026-06-15 17:22:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,36 @@ st.divider()
 
 df = pd.read_csv("data/yc_heritage_detail_enriched.csv")
 st.dataframe(df)
+
+
+st.sidebar.header("필터")
+
+# 시도코드 선택
+sido_list = ["전체"] + sorted(df["시도코드"].unique().tolist())
+selected_sido = st.sidebar.selectbox(
+    "시도코드 선택",
+    sido_list
+)
+
+# 국가유산종목 선택
+heritage_type_list = ["전체"] + sorted(df["국가유산종목"].unique().tolist())
+selected_type = st.sidebar.selectbox(
+    "국가유산종목 선택",
+    heritage_type_list
+)
+
+# ===== 필터 적용 =====
+filtered_df = df.copy()
+
+if selected_sido != "전체":
+    filtered_df = filtered_df[
+        filtered_df["시도코드"] == selected_sido
+    ]
+
+if selected_type != "전체":
+    filtered_df = filtered_df[
+        filtered_df["국가유산종목"] == selected_type
+    ]
+
+# 결과 출력
+st.dataframe(filtered_df)
